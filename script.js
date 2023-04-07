d1557b71bcf0d5717e98a1edbd71ef41fd961cdf
sayak-12 <sayakraha12@gmail.com>
2023-04-07 19:50:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const spans = document.querySelectorAll(".logospan span");
+console.log(spans);
+let firstSpan = spans[0];
+var text = "SayakRaha.";
+var i = 0;
+function typename() {
+  for (i = 0; i < text.length; i++) {
+    delaytype(i);
+  }
+}
+function delaytype(i) {
+  setTimeout(() => {
+    console.log(text[i]);
+    firstSpan.innerHTML += text[i];
+  }, 300 * i);
+}
+function deletename() {
+  for (i = 0; i < text.length; i++) {
+    delaydelete(i);
+  }
+}
+function delaydelete(i) {
+  setTimeout(() => {
+    console.log(text[i]);
+    firstSpan.innerHTML = firstSpan.innerHTML.substring(0, text.length-i-1);
+  }, 300 * i);
+}
+
+function animName() {
+  if (firstSpan.innerHTML === "") {
+     setTimeout(typename, 300);
+     
+    }
+  else if(firstSpan.innerHTML ==="SayakRaha."){
+      setTimeout(deletename, 300);
+    }
+  
+}
+animName();
+document.getElementById("click").addEventListener("click", ()=>{
+  animName();
+})
+setInterval(()=>{
+  document.getElementById("click").click();
+}, 3310)
+document.querySelector(".nav").addEventListener("", ()=>{
+  
+})
+// const timer = setInterval(typename, 1000);
