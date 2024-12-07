379d7e0566863193f14c40c3b9b064f03202a568
sayak raha <sayakraha12@gmail.com>
2024-12-07 15:05:37 +0530

    gsap implementation full

--- gsap.js
+++ gsap.js
@@ -1,3 +1,1 @@
-// dependencies.js
-document.write('<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>');
-document.write('<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>');
+
--- gsapinit.js
+++ gsapinit.js
@@ -1,3 +1,4 @@
+
 if (window.innerWidth >= 1124) {
   const timeline = gsap.timeline();
 timeline.fromTo(".socials", { 
@@ -33,4 +34,125 @@ timeline.fromTo(".socials", {
       duration: 0.2  
     }
   );
+  
 }
+
+gsap.fromTo(
+  "#proj1 .pic",
+  {
+    opacity: 0,
+    x:"-20%",
+    duration: 0.4,
+  },
+  {
+    opacity: 1,
+    x:"0%",
+    scrollTrigger: {
+      trigger: "#proj1",
+      start: "-10% 95%",
+      end: "30% 60%",
+      scrub:true,
+    }
+  }
+)
+gsap.fromTo(
+  "#proj1 .details p",
+  {
+    opacity: 0,
+    x:"20%",
+    duration: 0.4,
+  },
+  {
+    opacity: 1,
+    x:"0%",
+    scrollTrigger: {
+      trigger: "#proj1",
+      start: "-10% 95%",
+      end: "30% 60%",
+      scrub:true,
+    }
+  }
+)
+gsap.fromTo(
+  "#proj2 .pic",
+  {
+    opacity: 0,
+    x:"20%",
+    duration: 0.4,
+  },
+  {
+    opacity: 1,
+    x:"0%",
+    scrollTrigger: {
+      trigger: "#proj2",
+      start: "-10% 95%",
+      end: "30% 60%",
+      scrub:true,
+    }
+  }
+);
+gsap.fromTo(
+  "#proj2 .details1 p",
+  {
+    opacity: 0,
+    x:"-20%",
+    duration: 0.4,
+  },
+  {
+    opacity: 1,
+    x:"0%",
+    scrollTrigger: {
+      trigger: "#proj2",
+      start: "-10% 95%",
+      end: "30% 60%",
+      scrub:true,
+    }
+  }
+);
+gsap.fromTo(
+  "#proj3 .pic",
+  {
+    opacity: 0,
+    x:"-20%",
+    duration: 0.4,
+  },
+  {
+    opacity: 1,
+    x:"0%",
+    scrollTrigger: {
+      trigger: "#proj3",
+      start: "-10% 95%",
+      end: "30% 60%",
+      scrub:true,
+    }
+  }
+);
+gsap.fromTo(
+  "#proj3 .details p",
+  {
+    opacity: 0,
+    x:"20%",
+    duration: 0.4,
+  },
+  {
+    opacity: 1,
+    x:"0%",
+    scrollTrigger: {
+      trigger: "#proj3",
+      start: "-10% 95%",
+      end: "30% 60%",
+      scrub:true,
+    }
+  }
+);
+
+gsap.fromTo(".timeline",{"--top": "0%","--top-offset": "40px", duration: 1} ,{
+  "--top": "100%",
+  "--top-offset": "-40px",
+  scrollTrigger: {
+    trigger: ".timeline",
+    start: "30% 90%",
+    end: "bottom 20%",
+    scrub: true,
+  },
+});
